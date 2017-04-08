db2801af234e254823fa1242ed351ade1924d814
Antoine DUBAN <aduban@e1r12p18.42.fr>
2017-04-08 20:16:15 +0200

    save/load

--- Moviemon/game/data.py
+++ Moviemon/game/data.py
@@ -2,10 +2,13 @@ import requests
 import json
 from django.conf import settings
 import random
+import pickle
 
 class Data():
     movies = []
     player_strength = 0
+    score = 0
+    saves = [{'name': 'a', 'free': True, 'score' : 0},{'name': 'b', 'free': True, 'score' : 0},{'name': 'c', 'free': True, 'score' : 0}]
     def load_default_settings(self):
         for elem in settings.MOVIES:
             r = requests.get("http://www.omdbapi.com/?t=" + elem.replace(" ", "+"))
@@ -21,13 +24,28 @@ class Data():
                 self.movies = value
             elif key == 'player_strength':
                 self.player_strength = value
-    def dump(self, dic):
-        return {'movies': self.movies, 'player_strength' : self.player_strength}
+            elif key == 'score':
+                self.score = value
+            elif key == 'saves':
+                self.saves = value
+    def dump(self):
+        return {'movies': self.movies, 'player_strength' : self.player_strength, 'score': self.score, 'saves': self.saves}
     def get_random_movie(self):
         return random.choice(list(self.movies))
     def get_movie(self, title):
         for elem in self.movies:
             if elem['title'] == title:
                 return elem
+    def save(self, slot):
+        if slot == 'a' or slot == 'b' or slot == 'c':
+            pickle.dump(pickle.load(open("saved_game/tmp_save", "rb")), open("saved_game/slot{0}_{1}_15.mmg".format(slot, self.score), "wb+" ))
+            for elem in saves:
+                if elem['name'] == slot:
+                    elem['free'] = M#False
 
-
+    def load(self, slot):
+        if (slot == 'a' or slot == 'b' or slot == 'c'):
+            for elem in saves:
+                if elem['name'] == slot and elem['free'] == False:
+                    pickle.dump(pickle.load(open("saved_game/slot{0}_{1}_15.mmg".format(slot, self.score), "rb")),open("saved_game/tmp_save", "rb"))
+                    self.load(pickle.load(open("saved_game/tmp_save", "rb")))
