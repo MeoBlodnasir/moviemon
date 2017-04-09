75adc6bf98d3fff3f51012a4536ee21b42c3e3e6
Alexandre OLLIVIER <aollivie@e1r13p10.42.fr>
2017-04-09 16:20:54 +0200

    before pull

--- Moviemon/game/tool/game/fight.py
+++ Moviemon/game/tool/game/fight.py
@@ -12,16 +12,22 @@ class fight(Data):
         self.load_tmp()
         self.moviemon = self.get_movie(moviemon)
         self.is_captured = False
-        self.launch
+        self.launch = False
+        self.mess = ''
 
     def __str__(self):
         content = []
         content.append(e.Div(e.Text(str(self.moviemon['title']) + ' force: ' + str(self.moviemon['rating']) )))
         content.append(e.Div(e.Text('A - Launch movieball')))
         content.append(e.Div(Text('Player force: ' + str(battle.player_strength))))
+        if self.launch:
+
         return str(e.Div(content, attr={'class':'container'}))
 
     def set_moviemon(self, moviemon):
+        self.mess = ''
+        self.is_captured = False
+        self.launch = False
         self.load_tmp()
         self.moviemon = self.get_movie(moviemon)
 
@@ -36,6 +42,7 @@ class fight(Data):
     def launch_movieball(self):
         if self.player_strength == 0:
             return
+        self.launch = True
         c = self.chance_to_catch()
         r = random(0, 100)
         if r <= c:
